2c1eb572866b4eb1273a0e46b8a4594dd9020d31
Mehedi <mjimehedi99@gmail.com>
2025-01-08 00:54:11 +0600

    🐛 Name print in rust
    Nothihg

--- src/bin/a1.rs
+++ src/bin/a1.rs
@@ -1,3 +1,13 @@
+
+fn first_name() {
+    println!("Mehedi"); // This is a comment
+}
+
+fn last_name() {
+    println!("Hasan"); // This is a comment
+}
 fn main() {
     println!("Hello, world gfh!"); // This is a comment
+    first_name();
+    last_name();
 }
