16c8e974b5b63f843bf322f339793b781674c65a
Mehedi <mjimehedi99@gmail.com>
2025-01-08 01:07:54 +0600

    🚨 Arithmetic operation Using Rust completed
    That was easy

---
+++ src/bin/arithmetic.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let a = 10;
+    let b = 20;
+    let sum = a + b;
+    let sub = a - b;
+    let mul = a * b;
+    let div = a / b;
+    let rem = a % b;
+    println!("Sum: {}", sum);
+    println!("Sub: {}", sub);
+    println!("Mul: {}", mul);
+    println!("Div: {}", div);
+    println!("Rem: {}", rem);
+}
