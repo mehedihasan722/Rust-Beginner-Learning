c9e91a461ac792b77e4101d8a7fda3c22b64b128
Mehedi <mjimehedi99@gmail.com>
2025-01-08 01:12:38 +0600

    ✅ Arithmetic operation using Function in Rust
    Let's go rusty

--- src/bin/arithmetic.rs
+++ src/bin/arithmetic.rs
@@ -1,14 +1,30 @@
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+fn mul(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+fn div(a: i32, b: i32) -> i32 {
+    a / b
+}
+
+fn rem(a: i32, b: i32) -> i32 {
+    a % b
+}
+
 fn main() {
     let a = 10;
     let b = 20;
-    let sum = a + b;
-    let sub = a - b;
-    let mul = a * b;
-    let div = a / b;
-    let rem = a % b;
-    println!("Sum: {}", sum);
-    println!("Sub: {}", sub);
-    println!("Mul: {}", mul);
-    println!("Div: {}", div);
-    println!("Rem: {}", rem);
+
+    println!("Sum: {}", add(a, b));
+    println!("Sub: {}", sub(a, b));
+    println!("Mul: {}", mul(a, b));
+    println!("Div: {}", div(a, b));
+    println!("Rem: {}", rem(a, b));
 }
