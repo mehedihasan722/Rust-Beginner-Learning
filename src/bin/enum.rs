2958a57367ae08fd275bd6a2a1855cad6808bc6c
Mehedi <mjimehedi99@gmail.com>
2025-01-09 11:23:00 +0600

    📌 Enum statement in Rust Completed
    Let's go rusty

--- src/bin/enum.rs
+++ src/bin/enum.rs
@@ -2,17 +2,21 @@ fn main() {
     enum Age {
         UnderAge,
         Adult,
+        Child,
     }
 
-    let age: i32 = 30;
+    let age: i32 = 3;
     let person: Age = if age >= 21 {
         Age::Adult
-    } else {
+    } else if age < 21 && age >= 18 {
         Age::UnderAge
+    } else {
+        Age::Child
     };
 
     match person {
         Age::Adult => println!("You are old enough to drink"),
         Age::UnderAge => println!("You are not old enough to drink"),
+        Age::Child => println!("You are a child"),
     }
 }
