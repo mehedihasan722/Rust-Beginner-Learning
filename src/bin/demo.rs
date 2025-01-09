9420fac5f307dc45a7e2b87ef14a9714906f72ec
Mehedi <mjimehedi99@gmail.com>
2025-01-09 09:59:29 +0600

    ⚡️ Demo rust completed
    dfsgsj

---
+++ src/bin/demo.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let age: i32 = 30;
+
+    if age >= 21 {
+        println!("You are old enough to drink");
+    } else {
+        println!("You are not old enough to drink");
+    }
+}
