a6a8227132eba4949a0a8303411710899a4e9b17
Mehedi <mjimehedi99@gmail.com>
2025-01-10 12:15:52 +0600

    🧪 Tuples in Rust
    gfgh

---
+++ src/bin/tuples.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+enum Access {
+    Full,
+}
+fn one_two_three() -> (i32, i32, i32) {
+    (1, 2, 3)
+}
+fn main() {
+    let access: Access = Access::Full;
+    let numbers: (i32, i32, i32) = one_two_three();
+    let (one, two, three) = one_two_three();
+    println!("Access: {:?}", access);
+    println!("Numbers: {:?}", numbers);
+    println!("One: {}", one);
+    println!("Two: {}", two);
+    println!("Three: {}", three);
+}
