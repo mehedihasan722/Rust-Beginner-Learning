9cb9a7dd3712578c14d86d04038f9f4da888ec37
Mehedi <mjimehedi99@gmail.com>
2025-01-11 09:18:43 +0600

    🧵 Ownership in Rust
    fg

--- src/bin/expression1.rs
+++ src/bin/expression1.rs
@@ -4,9 +4,23 @@ fn print_message(is_greater_than_100: bool) {
         false => println!("The value is not greater than 100"),      
     }
 }
+enum Light {
+    Bright,
+    Dull,
+}
 
+fn display_light(light: &Light) {
+    match light {
+        Light::Bright => println!("The light is bright"),
+        Light::Dull => println!("The light is dull"),
+    }
+}
 fn main() {
     let value: i32 = 105;
     let is_greater_than_100: bool = value > 100;
     print_message(is_greater_than_100);
+    let dull_light: Light = Light::Dull;
+    display_light(&dull_light);
+    display_light(&dull_light);
+
 }
